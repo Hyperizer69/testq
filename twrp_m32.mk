becd2eafdbb1821c8d6c139ee3938a39039293b0
Hyper <98272700+Hyperizer69@users.noreply.github.com>
2022-02-17 07:02:33 +0530

    Add files via upload

---
+++ twrp_m32.mk
@@ -0,0 +1,19 @@
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Include any options that can't be included in BoardConfig.mk
+$(call inherit-product, device/samsung/m32/device.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := twrp_m32
+PRODUCT_DEVICE := m32
+PRODUCT_MODEL := SM-M325F
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
